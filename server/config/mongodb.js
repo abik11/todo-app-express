edc3ceb9e1bb17ba9cef152b6ff7fa2ee4fdee1e
abik11 <abik11@gmail.com>
2019-03-25 00:05:48 +0100

    Update mongodb.js

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -2,7 +2,7 @@ const mongodb = require('mongodb');
 
 module.exports = async () => {
     const connection = await mongodb.MongoClient.connect
-        ('mongodb+srv://readwrite:8KxPsqCwyGZ0tSkY@cluster0-vxk3t.mongodb.net/test?retryWrites=true', 
+        ('mongodb+srv://user:pass@cluster.mongodb.net/test?retryWrites=true', 
         { useNewUrlParser: true });
         
     return await connection.db('test-db');
