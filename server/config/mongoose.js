dbb1204413d9e2a14ed726284c6794f4f4df03d7
abik11 <abik11@gmail.com>
2019-03-28 19:00:08 +0100

    Adding dotenv and cross-env

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require( "mongoose" );
 require('../models/user');
+//require('../models'); //<- this is how it should be!
 
 mongoose
     .connect(process.env.MONGO, { useNewUrlParser: true })
     .then(db => console.log('[MongoDB connected]'))
-    .catch(error => console.error(error));
+    .catch(error => console.error(error)); //here put next(error)
--- server/index.js
+++ server/index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const app = express();
-const port = 5000;
 
+if(process.env.NODE_ENV != 'production')
+    require('dotenv').config();
+
+app.set('port', process.env.APP_PORT || 5001);
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 require('./config/mongoose');
 require('./config/multer')(app);
 require('./routes')(app);
 
-app.listen(port, () => console.log(`[Server running on port: ${[port]}]`));
+app.listen(app.get('port'), 
+    () => console.log(`[Server running on port: ${[app.get('port')]}]`));
